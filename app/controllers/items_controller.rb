d5fb55dbd0f32b751e4a41b5bd4fbdba0562fafa
Nulsea <wm7mw7@gmail.com>
2021-02-07 16:47:54 +0900

    カード登録ページマークアップ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,14 @@ class ItemsController < ApplicationController
 
   def new
   end
-end
 
+  def buyconfirm
+  end 
+  def card_new
+    @years=[]
+    11.times do |index|
+      this_year=Date.today.year%100
+      @years.push(this_year+index)
+    end
+  end
+end
